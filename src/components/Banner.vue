f97f34fcc970e72286df39d493b56f8cd367957c
marshall chen <marshallchan666@outlook.com>
2021-06-23 04:08:16 +0800

    banner component done

---
+++ src/components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-5 mt-10 border-2 border-white justify-self-center rounded-xl w-3/6 border-opacity-90 flex justify-between">
+        <img src="../assets/images/logo-bonus.svg" alt="">
+        <div class="bg-white py-3 px-8 rounded-xl text-center">
+            <p class="text-lg font-semibold text-blue-800 tracking-wider font-mono">SCORE</p>
+            <p class="text-6xl font-bold text-gray-800 tracking-tighter ">12</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup() {
+        const score = ref('')
+    }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
   },
   variants: {
     extend: {},
