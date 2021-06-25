1b00801de346dedac06a43d129422ffe4044c4a7
marshall chen <marshallchan666@outlook.com>
2021-06-25 15:50:40 +0800

    banner responsive design done

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="p-5 mt-10 border-2 border-white justify-self-center rounded-xl w-3/6 border-opacity-90 flex justify-between">
-        <img src="../assets/images/logo-bonus.svg" alt="logo">
-        <div class="bg-white py-3 px-12 rounded-xl text-center">
-            <p class="text-lg font-semibold text-blue-800 tracking-wider font-mono">SCORE</p>
-            <p class="text-6xl font-bold text-gray-800 tracking-tighter ">{{ score }}</p>
+    <div class="p-5 mt-10 border-2 border-white justify-self-center rounded-xl w-full md:w-3/6 border-opacity-90 flex justify-between">
+        <img src="../assets/images/logo-bonus.svg" alt="logo" class="h-20 md:h-auto">
+        <div class="bg-white py-3 px-6 md:px-12 rounded-xl text-center">
+            <p class="text-sm md:text-lg font-semibold text-blue-800 tracking-wider font-mono">SCORE</p>
+            <p class="text-xl md:text-6xl font-bold text-gray-800 tracking-tighter ">{{ score }}</p>
             <p @click="handleClick" class="text-xs uppercase text-blue-800 tracking-wider font-mono cursor-pointer">refresh</p>
         </div>
     </div>
@@ -19,12 +19,10 @@ export default {
      
         watchEffect (() => {
             score.value = localScore.value
-            console.log('watcheffect')
         })
 
         watch(localScore, () => {
             score.value = localScore.value
-            console.log('watch')
         })
 
         setTimeout(() => {
